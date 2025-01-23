72bf3d7e6f71d5d70a37716e2b81c777e60d6941
evanchooly <jlee@antwerkz.com>
2025-01-22 19:45:05 -0500

    fix the autoboxing examples

--- underthehood/src/main/java/com/antwerkz/underthehood/AutoBoxing.java
+++ underthehood/src/main/java/com/antwerkz/underthehood/AutoBoxing.java
@@ -1,15 +1,15 @@
 package com.antwerkz.underthehood;
 
 public class AutoBoxing {
-    public long math(long a, long b) {
-//    public Long math(Long a, long b) {
+    public static long math(long a, long b) {
+//    public static Long math(Long a, long b) {
         return a + b;
     }
 
-    public void demo() {
-        math(42, 2);
-//        math(42L, 2);
-//        math(Integer.valueOf(42), 2);
-//        math(Long.valueOf(42), 2);
+    public static void demo(long a, long b) {
+//    public static void demo(long a, Long b) {
+        long sum =
+//        Long sum =
+            math(a, b);
     }
 }
